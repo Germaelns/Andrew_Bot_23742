70fcbdca036926bab93e6587d588b49b2985dfe4
Nikolay <nikolay.shevchenko.work@gmail.com>
2019-05-21 11:08:10 +0300

    Change post timer

--- dal.py
+++ dal.py
@@ -68,7 +68,7 @@ class BotDatabaseController:
 
     @staticmethod
     def change_post_iter_time(session, minutes: str):
-        session.query(models.Variable).filter(models.Variable.description == "update_iter_time").update({'value': minutes*60})
+        session.query(models.Variable).filter(models.Variable.description == "post_iter_time").update({'value': minutes})
 
     @staticmethod
     def get_post_iter_time(session):
