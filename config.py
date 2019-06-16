4e0cba88aa7e87726a7b32dfbf5aa940f0bafcca
Nikolay <nikolay.shevchenko.work@gmail.com>
2019-06-16 08:49:27 +0300

    Test post

--- config.py
+++ config.py
@@ -1,21 +1,25 @@
 
 # Telegram bot
 
-TELEGRAM_TOKEN = '754428059:AAFYwBQYdp2k2K1TRQcFCnZIJ3RKmk1pOng'
-# TELEGRAM_TOKEN = '845427312:AAHj00GEHI0RVNiYPE4eh6QKXt-9qaJmsHU'
-TELEGRAM_CHAT_ID = '@trash7289563405'
-# TELEGRAM_CHAT_ID = '@top_sale_ua'
+#trash-token
+# TELEGRAM_TOKEN = '754428059:AAFYwBQYdp2k2K1TRQcFCnZIJ3RKmk1pOng'
+#post-topsale-token
+TELEGRAM_TOKEN = '845427312:AAHj00GEHI0RVNiYPE4eh6QKXt-9qaJmsHU'
+#trash-chat
+# TELEGRAM_CHAT_ID = '@trash7289563405'
+#post-topsale-chat
+TELEGRAM_CHAT_ID = '@top_sale_ua'
 
 # VK API
 
-VK_APP_TOKEN = 'b42f8401b42f8401b42f8401e0b44504ebbb42fb42f8401e8f91a4da1bad7811ea125e6'
-# VK_APP_TOKEN = '6e1674f76e1674f76e1674f7346e7cda9466e166e1674f732f5e3e68ed810311987bd05'
+#trash-vk
+# VK_APP_TOKEN = 'b42f8401b42f8401b42f8401e0b44504ebbb42fb42f8401e8f91a4da1bad7811ea125e6'
+# post-topsale-ck
+VK_APP_TOKEN = '6e1674f76e1674f76e1674f7346e7cda9466e166e1674f732f5e3e68ed810311987bd05'
 
 # CPA
 
 
 # PostgreSQL
-
-# POSTGRE_URI = 'postgres://gwotcibrvpvoje:092cf569b2d7f90ed4240ae3b5bec3c8359a5882b5ae42ce24a99125aa3cf78b@ec2-54-75-230-41.eu-west-1.compute.amazonaws.com:5432/d45lnaal54p8mv'
 POSTGRE_URI = 'postgres://atocrwzynmildu:95f07d735d5189c0624fb5c56654ca41a07572e146eda2a67d752031f5f74737@ec2-54-235-86-101.compute-1.amazonaws.com:5432/d87hcpedklai2t'
 
